7f9515c570c8537cbece97ed039596ae4f0e35a1
manojselvang <144434478+manojselvang@users.noreply.github.com>
2026-03-15 02:25:50 +0530

    Correct syntax and logic in backup_script.sh
    Fix syntax errors and improve backup script logic.

--- backup_script.sh
+++ backup_script.sh
@@ -7,6 +7,7 @@ DATABASE_NAME="production_db"
 BACKUP_DATE=$(date +%Y%m%d)
 BACKUP_DIR="/home/ubuntu/backup/date_$BACKUP_DATA"
 BACKUP_FILE="$BACKUP_DIR/${DATABASE_NAME}_BACKUP.TXT"
+LARGE_FILE=10000
 
 echo "Checking if $BACKUP_DIR directory exists"
 
@@ -28,4 +29,27 @@ else
   exit 1
 fi
 
-if [-e "$DATABASE_BASE
+if [-e "$DATABASE_NAME"]; then
+  echo "Database found, Starting backup."
+  # Backup command
+
+  FILE_SIZE=ls -l $BACKUP_FILE | awk '{print $ 5}'
+
+  if [$FILE_SIZE -gt $LARGE_FILE]; the
+    echo "Large file found"
+  else
+    echo "Smail file found"
+  fi
+else
+  echo "Error! Database Not found"
+  exit 1
+fi
+echo "Backup of Database Complete"
+
+echo "Checking if backuped file exists"
+if [-e $BACKUP_FILE]; THEN
+  echo "Backup file exists"
+ else
+   echo "Backup file doesn't exists"
+   exit 1
+  fi
