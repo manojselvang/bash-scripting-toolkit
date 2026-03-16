41885f1d0c3bd97f7c01d28d869b65beac3b5c77
manojselvang <144434478+manojselvang@users.noreply.github.com>
2026-03-17 00:24:44 +0530

    Correct echo command and enhance script clarity
    Fix typo in echo command and improve script comments.

--- handle_kill_signal.sh
+++ handle_kill_signal.sh
@@ -1,21 +1,25 @@
+# Below script shows a simle example of handling kill signals using Trap command
 
 #!/bin/bash
 
 $$              # Fetch PID of current Shell
 
 cleanup() {
-  eco "deleting temp files"
+  echo "deleting temp files"
   rm -f /temp/temp_file.txt
   echo "Cleanup Complete"
 }
 
+# Run cleanup when SIGTERM or SIGINT signal is received
 trap 'echo "kill signal received, cleaning up.."; cleanup; exit 1; TERM INT
-echo "some long running process"
-tough /temp/temp_file.txt
 
-# long running task
+echo "Starting long running process"
+touch /temp/temp_file.txt      # Create temp file
+
+# Simple long running task
 while true; do
   echo "Some long running task"
   sleep 2
 done
+
 cleanup
