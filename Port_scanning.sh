9669fa09b6ab6d21cb3515989e26f335376d3bcb
manojselvang <144434478+manojselvang@users.noreply.github.com>
2026-03-24 23:12:00 +0530

    Update Port_scanning.sh

--- Port_scanning.sh
+++ Port_scanning.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-
 TARGET="192.168.1.10"
 
 for port in {1..1024}
 do
-  nc -zvw1 $TARGET $port 2>/dev/null && echo "Port $port OPEN"
+  if nc -zvw1 $TARGET $port 2>/dev/null
+  then
+    echo "[OPEN]  Port $port"
+  else
+    echo "[CLOSED] Port $port"
+  fi
 done
